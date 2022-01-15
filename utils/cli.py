88ad39b57a405ee3f4eb44aaf6fae5b605414329
Courrier Guillaume <courrier.guillaume.123@gmail.com>
2022-01-15 10:28:07 +0100

    kanban: add projects command to list projects

--- utils/cli.py
+++ utils/cli.py
@@ -1,4 +1,4 @@
-from .taskwarrior import list_domains
+from .taskwarrior import list_domains, list_projects
 from argparse import ArgumentParser
 
 
@@ -6,6 +6,12 @@ def exec_domains():
     domains = list_domains()
     for domain in domains:
         print(domain)
+
+
+def exec_projects():
+    projects = list_projects()
+    for project in projects:
+        print(project)
 
 
 def dispatch_action(action: str, parser: ArgumentParser):
