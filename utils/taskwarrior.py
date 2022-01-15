6f4fdc3c0e35edd6233c14055c846b415e88732d
Courrier Guillaume <courrier.guillaume.123@gmail.com>
2022-01-15 08:50:23 +0100

    refacto: prepare list_projects

--- utils/taskwarrior.py
+++ utils/taskwarrior.py
@@ -1,13 +1,19 @@
 import subprocess
 
+
 def run_task(*args: str):
     return subprocess.run(['task', *args],
                           capture_output=True,
                           text=True)
 
-def list_domains():
-    domains = run_task('_projects')
-    if len(domains.stdout) > 0:
-        return domains.stdout.strip('\n').split('\n')
+
+def list_from_newline(output: str):
+    if len(output) > 0:
+        return output.strip('\n').split('\n')
     else:
         return []
+
+
+def list_domains():
+    domains = run_task('_projects')
+    return list_from_newline(domains.stdout)
