201956170248a52364b24b079032af8b8379eb5d
Courrier Guillaume <courrier.guillaume.123@gmail.com>
2022-01-15 10:11:39 +0100

    project: add list_projects function

--- tests/acceptance.py
+++ tests/acceptance.py
@@ -2,7 +2,7 @@ import unittest
 import sys
 import os
 
-from utils.taskwarrior import list_domains, run_task
+from utils.taskwarrior import list_domains, run_task, list_projects
 
 class TestTaskwarriorExports(unittest.TestCase):
 
@@ -11,6 +11,18 @@ class TestTaskwarriorExports(unittest.TestCase):
         run_task('1', 'delete')
         run_task('2', 'delete')
         run_task('3', 'delete')
+
+    def test_one_project(self):
+        run_task('add', '+one', 'task description')
+        self.assertEqual(list_projects(), ['one'])
+
+    def test_projects(self):
+        run_task('add', '+one', 'task description')
+        run_task('add', '+two', 'task description')
+        run_task('add', '+three', 'task description')
+
+        self.assertEqual(sorted(list_projects()),
+                         sorted(['one', 'two', 'three']))
 
     def test_domains(self):
         run_task('add', 'project:one', 'task description')
@@ -34,6 +46,8 @@ class TestTaskwarriorExports(unittest.TestCase):
         self.assertEqual(run_task('_projects').returncode, 0)
 
 if __name__ == '__main__':
-    root = os.getenv('PYTHONPATH')
-    os.environ['TASKRC'] = os.path.join(root, 'tests', '.taskrc')
+    if os.getenv('TASKRC') is None:
+        testdir = os.getenv('TESTDIR')
+        os.environ['TASKRC'] = os.path.join(testdir, '.taskrc')
+
     unittest.main()
--- utils/taskwarrior.py
+++ utils/taskwarrior.py
@@ -1,6 +1,27 @@
 import subprocess
 
 
+default_tags = {
+  'ACTIVE', 'ANNOTATED',
+  'BLOCKED', 'BLOCKING',
+  'CHILD', 'COMPLETED',
+  'DELETED', 'DUE',
+  'DUETODAY', 'INSTANCE',
+  'LATEST', 'MONTH',
+  'ORPHAN', 'OVERDUE',
+  'PARENT', 'PENDING',
+  'PRIORITY', 'PROJECT',
+  'QUARTER', 'READY',
+  'SCHEDULED', 'TAGGED',
+  'TEMPLATE', 'TODAY',
+  'TOMORROW', 'UDA',
+  'UNBLOCKED', 'UNTIL',
+  'WAITING', 'WEEK',
+  'YEAR', 'YESTERDAY',
+  'next', 'nocal',
+  'nocolor', 'nonag',
+}
+
 def run_task(*args: str):
     return subprocess.run(['task', *args],
                           capture_output=True,
@@ -17,3 +38,9 @@ def list_from_newline(output: str):
 def list_domains():
     domains = run_task('_projects')
     return list_from_newline(domains.stdout)
+
+
+def list_projects():
+    projects = run_task('_tags')
+    tags = list_from_newline(projects.stdout)
+    return list(set(tags).difference(default_tags))
