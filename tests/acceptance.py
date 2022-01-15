da4b0e7219797fbc423d0aa8342d386c411c1bdb
Courrier Guillaume <courrier.guillaume.123@gmail.com>
2022-01-15 07:57:09 +0100

    test: run tests in an isolated environment

--- tests/acceptance.py
+++ tests/acceptance.py
@@ -2,12 +2,23 @@ import unittest
 import sys
 import os
 
-from utils.taskwarrior import test
+from utils.taskwarrior import list_domains, run_task
 
 class TestTaskwarriorExports(unittest.TestCase):
 
-    def test_list_projects(self):
-        self.assertEqual(test().upper(), 'FOO')
+    def test_run_task(self):
+        self.assertEqual(run_task('_projects').returncode, 0)
+
+    def test_empty_session(self):
+        """Make sure that we are not modifying the system task list"""
+        if list_domains() != []:
+            self.fail("The tests are not run on an empty instance, "
+                    "check your config at tests/.taskrc")
+
+    def test_one_domain(self):
+        pass
 
 if __name__ == '__main__':
+    root = os.getenv('PYTHONPATH')
+    os.environ['TASKRC'] = os.path.join(root, 'tests', '.taskrc')
     unittest.main()
--- utils/taskwarrior.py
+++ utils/taskwarrior.py
@@ -1,2 +1,13 @@
-def test():
-    return "foo"
+import subprocess
+
+def run_task(*args: str):
+    return subprocess.run(['task', *args],
+                          capture_output=True,
+                          text=True)
+
+def list_domains():
+    domains = run_task('_projects')
+    if len(domains.stdout) > 0:
+        return domains.stdout.split('\n')
+    else:
+        return []
