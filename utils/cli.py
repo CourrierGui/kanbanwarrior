b113d2ca3deff645e6188c30c8ed0b7eed4c274f
Courrier Guillaume <courrier.guillaume.123@gmail.com>
2022-01-16 09:45:46 +0100

    cli: add page command to generate static HTML table

--- utils/cli.py
+++ utils/cli.py
@@ -1,17 +1,26 @@
-from .taskwarrior import list_domains, list_projects
+from . import taskwarrior as tw
+from . import task_converter as tc
+
 from argparse import ArgumentParser
 
 
 def exec_domains():
-    domains = list_domains()
+    domains = tw.list_domains()
     for domain in domains:
         print(domain)
 
 
 def exec_projects():
-    projects = list_projects()
+    projects = tw.list_projects()
     for project in projects:
         print(project)
+
+
+def exec_page():
+    tasks = tw.list_pending()
+    table = tc.tasks_to_table(tasks)
+
+    print(table.dump())
 
 
 def dispatch_action(action: str, parser: ArgumentParser):
