1f83ab8958a9f4cb2395a04ee045d280c408bb86
Courrier Guillaume <courrier.guillaume.123@gmail.com>
2022-03-12 22:23:24 +0100

    style: use jinja template to generate a page

--- server.py
+++ server.py
@@ -1,126 +1,40 @@
 from flask import Flask
-from utils import cli
+
 from utils import taskwarrior as tw
-from utils import task_converter as tc
-from utils import _html
+from utils.kanban import KanbanPage
 
 
 app = Flask(__name__)
 
 
-def build_domain_table(domains: list[str]) -> _html.Table:
-    table = _html.Table()
+def build_page(action: str, value: str = '') -> KanbanPage:
+    page = KanbanPage()
 
-    header = table.make_header()
-    header.insert('Domains')
-
-    for domain in domains:
-        row = table.add_row()
-        a = _html.Anchor('/domains/'+domain,
-                         _html.Button(_html.Text(domain)))
-        row.insert_node(a)
-
-    return table
-
-
-def build_project_table(projects: list[str]) -> _html.Table:
-    table = _html.Table()
-
-    header = table.make_header()
-    header.insert('Projects')
-
-    for project in projects:
-        row = table.add_row()
-        a = _html.Anchor('/projects/' + project,
-                         _html.Button(_html.Text(project)))
-        row.insert_node(a)
-
-    return table
-
-
-def build_kanban_board(action: str, value: str) -> _html.Table:
     if action == 'projects':
-        query = '+' + value
+        query = '+' + value if value is not '' else ''
+        page.projects(tw.list_projects())
     elif action == 'domains':
-        query = 'project:' + value
-    else:
-        return None
+        query = 'pro:' + value if value is not '' else ''
+        page.projects(tw.list_projects(value))
 
-    todo = cli.table_from_query(query, 'status:pending', '-ACTIVE',
-                                '-inbox', 'export')
-    inprogress = cli.table_from_query(query, 'export', 'active')
-    done = tw.list_tasks(query, 'end.after:today-1wk', 'export',
-                         'completed')
-    done = tc.tasks_to_table(done, id=False)
+    page.domains(tw.list_domains())
 
-    table = _html.Table()
-    header = table.make_header()
-    row = table.add_row(align_top=True)
+    page.inbox(tw.list_tasks('status:pending', '-ACTIVE', '+inbox', 'export'))
+    page.todo(tw.list_tasks(query, 'status:pending', '-ACTIVE', '-inbox', 'export'))
+    page.inprogress(tw.list_tasks(query, 'export', 'active'))
+    page.done(tw.list_tasks(query, 'end.after:today-1wk', 'export'))
 
-    header.insert('TODO')
-    row.insert_node(todo)
-
-    header.insert('In Progess')
-    row.insert_node(inprogress)
-
-    header.insert('Done')
-    row.insert_node(done)
-
-    return table
-
-
-def build_page(action: str, value: str) -> _html.Table:
-    main = _html.Table()
-    rows = main.add_row(align_top=True)
-    kanban = build_kanban_board(action, value);
-
-    inbox_table = _html.Table()
-    inbox_header = inbox_table.add_row()
-    inbox_header.insert('<b>Inbox</b>')
-    inbox_content = inbox_table.add_row()
-    inbox_content.insert_node(cli.table_from_query('status:pending', '-ACTIVE',
-                                                   '+inbox', 'export'))
-
-    rows.insert_node(inbox_table)
-    rows.insert_node(build_domain_table(tw.list_domains()))
-    if action == 'domains':
-        rows.insert_node(build_project_table(tw.list_projects(value)))
-
-
-    if action == 'domains' and kanban:
-        rows.insert_node(kanban)
-    elif action == 'projects':
-
-        sub = _html.Table()
-        project = sub.add_row()
-        project.insert('<b>Project:</b> ' + value)
-        k = sub.add_row()
-        if kanban:
-            k.insert_node(kanban)
-
-        rows.insert_node(sub)
-    else:
-        return None
-
-
-    page = _html.Page('My Kanban', css='/static/styles/style.css')
-    page.insert(main)
     return page
 
 
 @app.route('/')
 def get_backlog():
-    table = cli.build_page()
-    return table.dump() if table else 'page not found'
+    return build_page('domains').html()
 
 
 @app.route('/<string:action>/<string:name>')
 def get_project_backlog(action: str, name: str):
-    table = build_page(action, name)
-    if not table:
-        return 'page not found: /' + action + '/' + name
-    else:
-        return table.dump()
+    return build_page(action, value).html()
 
 
 if __name__ == '__main__':
---
+++ utils/kanban.py
@@ -0,0 +1,47 @@
+from jinja2 import FileSystemLoader, Environment
+
+
+class KanbanPage:
+
+    def __init__(self):
+        self._domains = dict()
+
+    def domains(self, values: list[str]) -> None:
+        for domain in values:
+            subdomains = domain.split('.')
+            if subdomains[0] not in self._domains:
+                self._domains[subdomains[0]] = []
+
+            # FIXME add support for nested domains?
+            if len(subdomains) == 2:
+                self._domains[subdomains[0]].append(subdomains[1])
+            elif len(subdomains) > 2:
+                self._domains[subdomains[0]].append('.'.join(subdomains[1:]))
+
+    def projects(self, values: list[str]) -> None:
+        self._projects = values
+
+    def inbox(self, values: list[str]) -> None:
+        self._inbox = values
+
+    def todo(self, values: list[str]) -> None:
+        self._todo = values
+
+    def inprogress(self, values: list[str]) -> None:
+        self._inprogress = values
+
+    def done(self, values: list[str]) -> None:
+        self._done = values
+
+    def html(self) -> str:
+        file_loader = FileSystemLoader('.')
+        env = Environment(loader=file_loader)
+
+        template = env.get_template('template.html')
+        return template.render(title='My Kanban',
+                               inbox=self._inbox,
+                               todo=self._todo,
+                               inprogress=self._inprogress,
+                               done=self._done,
+                               projects=self._projects,
+                               domains=self._domains)
